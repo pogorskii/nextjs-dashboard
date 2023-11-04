0effb012763baa783d09f293981633ec8c3301f4
Stanislav <stanislav.pogorskii@gmail.com>
2023-11-05 02:04:39 +0400

    updated

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,22 +1,3 @@
-// import { Metadata } from "next";
-// import { fetchCustomers } from "@/app/lib/data";
-// import Table from "@/app/ui/customers/table";
-// import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
-// import { Suspense } from "react";
-
-// export const metadata: Metadata = {
-//   title: "Customers",
-// };
-
-// export default async function Page() {
-//   const customers = await fetchCustomers();
-//   return (
-//     <Suspense fallback={<InvoicesTableSkeleton />}>
-//       <Table customers={customers} />
-//     </Suspense>
-//   );
-// }
-
 import Table from "@/app/ui/customers/table";
 import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
